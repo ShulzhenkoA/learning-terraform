99ef18f30d806686e274c8bab1318dc0687fa917
Andrii Shulzhenko <59138473+ShulzhenkoA@users.noreply.github.com>
2024-04-10 23:40:51 +0300

    Changed region

--- providers.tf
+++ providers.tf
@@ -7,5 +7,32 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-central-1"
 }
+
+#resource "aws_s3_bucket" "tf-course" {
+#  bucket = "my-first-bucket-111222333"
+#  acl = "private"
+#  policy = file("policy.json")
+#}
+
+#resource "aws_s3_bucket_website_configuration" "tf-course" {
+#  bucket = aws_s3_bucket.tf-course.bucket
+#  index_doucument {
+#    suffix = "index.html"
+#  }
+#}
+
+#resource "aws_vpc" "QA" {
+#  cidr_block = "10.0.0.0/16"
+#}
+
+#resource "aws_vpc" "Staging" {
+#  cidr_block = "10.0.0.0/16"
+#}
+
+#resource "aws_vpc" "Prod" {
+#  cidr_block = "10.0.0.0/16"
+#}
+
+# resource
